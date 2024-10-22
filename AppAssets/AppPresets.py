8927eb70442eed8394161170d830649a0c28c3ed
BenStacey236 <dealt-runner0c@icloud.com>
2024-10-22 20:53:36 +0100

    Adding assets required for the app

---
+++ AppAssets/AppPresets.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+
+# Colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (30, 30, 30)
+LIGHT_GREY = (60, 60, 60)
+V_LIGHT_GREY = (90, 90, 90)
+VV_LIGHT_GREY = (130, 130, 130)
+BASICALLY_WHITE = (200, 200, 200)
+DARK_GREY = (25, 25, 25)
+RED = (255, 0, 0)
+CYAN = (0, 255, 255)
+ORANGE = (255, 150, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+
+# Fonts
+TIMER_FONT_3D = pygame.font.SysFont('applesdgothicneo', 40)
+TIMER_FONT_2D = pygame.font.SysFont('applesdgothicneo', 80)
+SCRAMBLE_FONT = pygame.font.SysFont('applesdgothicneo', 50)
+SOLVES_FONT = pygame.font.SysFont('applesdgothicneo', 20)
+SOLVES_TITLE_FONT = pygame.font.SysFont('applesdgothicneo', 20, bold=True)
